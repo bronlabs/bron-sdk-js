4e026a981ee8e2ae7a2a5dc3d9f8ab59a5d1c4a6
Dmitry Kulikov <kulikov.dm@gmail.com>
2025-08-01 12:44:49 +0100

    api/assets: add get by ID methods for asset network and symbol

--- src/api/assets.ts
+++ src/api/assets.ts
@@ -1,11 +1,14 @@
 import { Assets } from "../types/Assets.js";
 import { AssetsQuery } from "../types/AssetsQuery.js";
+import { Asset } from "../types/Asset.js";
 import { Networks } from "../types/Networks.js";
 import { NetworksQuery } from "../types/NetworksQuery.js";
+import { Network } from "../types/Network.js";
 import { SymbolMarketPrices } from "../types/SymbolMarketPrices.js";
 import { PricesQuery } from "../types/PricesQuery.js";
 import { Symbols } from "../types/Symbols.js";
 import { SymbolsQuery } from "../types/SymbolsQuery.js";
+import { Symbol } from "../types/Symbol.js";
 import { HttpClient } from "../utils/http.js";
 
 export class AssetsAPI {
@@ -20,11 +23,25 @@ export class AssetsAPI {
     });
   }
 
+  async getAssetById(assetId: string): Promise<Asset> {
+    return this.http.request<Asset>({
+      method: "GET",
+      path: `/dictionary/assets/${assetId}`
+    });
+  }
+
   async getNetworks(query?: NetworksQuery): Promise<Networks> {
     return this.http.request<Networks>({
       method: "GET",
       path: `/dictionary/networks`,
       query
+    });
+  }
+
+  async getNetworkById(networkId: string): Promise<Network> {
+    return this.http.request<Network>({
+      method: "GET",
+      path: `/dictionary/networks/${networkId}`
     });
   }
 
@@ -41,6 +58,13 @@ export class AssetsAPI {
       method: "GET",
       path: `/dictionary/symbols`,
       query
+    });
+  }
+
+  async getSymbolById(symbolId: string): Promise<Symbol> {
+    return this.http.request<Symbol>({
+      method: "GET",
+      path: `/dictionary/symbols/${symbolId}`
     });
   }
 }
